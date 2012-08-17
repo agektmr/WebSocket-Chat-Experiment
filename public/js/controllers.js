551eb2c4d92695e52576335b109402677f355050
Eiji Kitamura <agektmr@gmail.com>
2012-08-17 17:08:21 +0900

    initial commit

---
+++ public/js/controllers.js
@@ -0,0 +1,112 @@
+/*
+Copyright 2012 Eiji Kitamura
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Eiji Kitamura (agektmr@gmail.com)
+*/
+'use strict';
+
+var WS_HOST = window.location.href.replace(/(http|https)(:\/\/.*?)\//, 'ws$2');
+
+var WebSocketChatCtrl = function($scope) {
+  $scope.session_standby = true;
+  $scope.session_button = 'connect';
+  $scope.name = '';  
+  $scope.attendees = [];
+  $scope.message = '';
+  $scope.messages = [];
+  $scope.notification = '';
+  $scope.notification_type = 'info';
+  $scope.toggle_session = function toggle_session() {
+    if (!$scope.session_standby) {
+      $scope.chat.close();
+      $scope.chat = null;
+    } else {
+      $scope.chat = new WebSocket(WS_HOST);
+      $scope.chat.onopen = $scope.onopen;
+      $scope.chat.onmessage = $scope.onmessage;
+      $scope.chat.onclose = $scope.onclose;
+      $scope.chat.onerror = $scope.onerror;
+      $scope.session_standby = false;
+      $scope.session_button = 'disconnect';
+    }
+  };
+  $scope.onopen = function() {
+    var msg = {
+      name: $scope.name,
+      type: 'connection',
+      message: ''
+    };
+    $scope.chat.send(JSON.stringify(msg));
+    $scope.notify('Welcome, '+$scope.name+'!', 'success');
+  };
+  $scope.onclose = function() {
+    $scope.messages = [];
+    $scope.attendees = [];
+    $scope.session_standby = true;
+    $scope.session_button = 'connect';
+    $scope.notify('You are disconnected', 'success');
+    $scope.$apply();
+  };
+  $scope.onerror = function() {
+
+  };
+  $scope.onmessage = function(req) {
+    var msg = JSON.parse(req.data);
+    switch (msg.type) {
+      case 'connection':
+        $scope.attendees = msg.message;
+        break;
+      case 'message':
+        $scope.messages.unshift(msg);
+        break;
+      default:
+        return;
+    }
+    $scope.$apply();
+  };
+  $scope.send_message = function() {
+    var message = $scope.message;
+    if (message.length > 0) {
+      var msg = {
+        'name': $scope.name,
+        'type': 'message',
+        'message': message
+      };
+      $scope.chat.send(JSON.stringify(msg));
+      $scope.message = '';
+      $scope.$apply();
+    }
+  };
+  $scope.notify = function(message, type, sticky) {
+    sticky = sticky || false;
+    type = type || 'info';
+    $scope.notification = message;
+    $scope.notification_type = type;
+    if (!sticky) {
+      setTimeout(function interval() {
+        $scope.notification = '';
+        $scope.$apply();
+      }, 3000);
+    }
+  };
+
+  document.querySelector('#text').onkeydown = function(e) {
+    if (e.keyCode == 13) {
+      $scope.send_message();
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  };
+};
---
+++ server.js
@@ -0,0 +1,141 @@
+/*
+Copyright 2012 Eiji Kitamura
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Eiji Kitamura (agektmr@gmail.com)
+*/
+'use strict';
+
+var express = require('express'),
+    routes  = require('./routes'),
+    WebSocketServer = require('ws').Server;
+
+var app = module.exports = express.createServer();
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+var SessionManager = function() {
+  this.user_index = 0;
+  this.connections = [];
+};
+SessionManager.prototype = {
+  addUser: function(name, socket) {
+    var user = {
+      user_id: this.user_index++,
+      name: name || 'No Name',
+      socket: socket
+    };
+    this.connections.push(user);
+    return true;
+  },
+  removeUser: function(socket) {
+    for (var i = 0; i < this.connections.length; i++) {
+      var user = this.connections[i];
+      if (user.socket == socket) {
+        this.connections.splice(i, 1);
+        return true;
+      }
+    };
+    return false;
+  },
+  getUserList: function() {
+    var list = [];
+    this.connections.forEach(function(user) {
+      list.push({
+        user_id: user.user_id,
+        name: user.name
+      });
+    });
+    return list;
+  }
+};
+
+app.listen(3010, function() {
+  var ws = new WebSocketServer({server:app});
+  var session = new SessionManager();
+
+  // Start accepting WebSocket connection
+  ws.on('connection', function(socket) {
+    // When a message is received
+    socket.on('message', function(req) {
+console.log('received', req);
+      var msg = JSON.parse(req);
+      switch (msg.type) {
+        case 'connection':
+          session.addUser(msg.name, socket);
+          var list = session.getUserList();
+          msg = {
+            type: 'connection',
+            message: list
+          };
+          break;
+        case 'message':
+          msg.datetime = (new Date()).getTime();
+          break;
+        default:
+          break;
+      }
+      broadcast(msg);
+    });
+
+    // When the connection is closed
+    socket.on('close', function() {
+      clearInterval(interval);
+      interval = null;
+
+      // remove session
+      session.removeUser(socket);
+      var list = session.getUserList();
+      var msg = {
+        type: 'connection',
+        message: list
+      };
+      broadcast(msg);
+    });
+
+    // When an error occurred
+    socket.on('error', function(e) {
+      console.log('Error: '+e.message);
+    });
+
+    var interval = setInterval(function() {
+      console.log('sending heartbeat.');
+      socket.ping();
+    }, 30*1000);
+  });
+
+  // Broadcast a message
+  var broadcast = function(msg) {
+    session.connections.forEach(function(user) {
+      msg = JSON.stringify(msg);
+console.log('sending', msg);
+      user.socket.send(msg);
+    });
+  };
+});
+
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
